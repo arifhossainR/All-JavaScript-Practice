d1a02a27cf83d85525250c6c91a5aa24b618650d
ArifHossainR <arifcse012@gmail.com>
2023-11-24 00:30:00 +0600

    first JavaScript practice

---
+++ app.js
@@ -0,0 +1,90 @@
+//alert("Alert message showing");
+// console.log(confirm("Are you sure?"));
+//  var name, food;
+//  nameki = prompt("Name plz: ");
+//  food = prompt("Food you like: ");
+
+
+// console.log("This is: "+ nameki +". He/she Loves: "+food);
+// var nameki = "I\'Arif Hossain";
+// console.log(` ${nameki} is Can\'t Very good boy`);
+
+
+// let nameki  = prompt("Enter your name: ");
+// let numbarr = prompt("Enter a number: ");
+// console.log(typeof parseInt(numbarr));
+
+
+// let hex = 0xff;
+// let octa = 012;
+// console.log(hex);
+// console.log(octa);
+
+
+// let a = 10;
+// let b = 20;
+// console.log((a==b));
+// console.log(a!=b);
+
+
+// let val = 40;
+// let val2 = '50';
+
+// console.log(val!==val2);
+
+// console.log(typeof("Arif"));
+
+// console.log(1 ^ 4);
+
+
+// let num = 6.7348;
+
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.round(num));
+// console.log(parseInt(Math.random()));
+// console.log(Math.abs(num));
+// console.log(Math.max(1, 2, 3));
+// console.log(Math.min(1, 2, 3));
+// console.log(Math.pow(2, 3));
+// console.log(Math.pow(2, 5));
+// console.log(Math.sqrt(25));
+
+// let dat = new Date();
+
+// console.log(dat.toDateString());
+// console.log(dat.toLocaleDateString());
+// console.log(dat.toLocaleString());
+// console.log(dat.toLocaleTimeString());
+// console.log(dat.toTimeString());
+
+// console.log(dat.getDate());
+// console.log(dat.getDay());
+// console.log(dat.getHours());
+// console.log(dat.getFullYear());
+// console.log(dat.getTime());
+// console.log(dat.getMonth());
+// console.log(dat.getSeconds());
+
+
+// let variable = new ItsContructor();
+
+// console.log(variable.name, variable.id, variable.registration, variable.subFunction1OfConstructor(), variable.subFunction2OfConstructor());
+
+
+// for(let i=1; i<=10; i++)
+// {
+//     for(let j=1; j<=10; j++)
+//     {
+//         console.log(i +" X "+ j + " = "+ (i*j));
+//     }
+//     console.log("\n");
+// }
+
+let name = "Arif Hossain"
+let vari = 10;
+
+console.log(namee);
+console.log(vari);
