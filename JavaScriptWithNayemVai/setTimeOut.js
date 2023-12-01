d9fb323c178232861881cae28115ec010d48249b
ArifHossainR <arifcse012@gmail.com>
2023-12-01 08:48:06 +0600

    moved setTimeout.js file from JavaScriptWithNayemVai to learnWithSumitDada

--- JavaScriptWithNayemVai/setTimeOut.js
+++
@@ -1,4 +0,0 @@
-setTimeout(function(){
-    console.log("Printing something 1");
-}, 3000)
-console.log("Printing something 2");
---
+++ learnWithSumitDada/setTimeOut.js
@@ -0,0 +1,9 @@
+console.log("Printing something 2");
+setTimeout(function(){
+    console.log("Printing something 1");
+}, 3000)
+setInterval(function(){
+    console.log("Printing something 4");
+}, 3000)
+
+console.log("Printing something 2");
