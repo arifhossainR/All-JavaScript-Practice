0d248e38b39f66ac0a2c65531e78595e1c829550
ArifHossainR <arifcse012@gmail.com>
2023-12-17 18:57:13 +0600

    DOM Navigation

---
+++ DOM/DOM Navigation/index.js
@@ -0,0 +1,4 @@
+const parent = document.getElementById("container").parentElement;
+console.log(parent);
+const child = document.getElementById("container").childNodes[1].childElementCount;
+console.log(child);
