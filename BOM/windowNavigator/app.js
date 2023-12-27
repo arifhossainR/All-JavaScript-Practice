ccf3936df98f5b6f793d38b32db3bb158e1a349a
ArifHossainR <arifcse012@gmail.com>
2023-12-27 19:29:48 +0600

    window navigator in JS ):

---
+++ BOM/windowNavigator/app.js
@@ -0,0 +1,11 @@
+document.querySelector("#appname").innerHTML = navigator.appName;
+document.querySelector("#appcodename").innerHTML = navigator.appCodeName;
+document.querySelector("#platform").innerHTML = navigator.platform;
+document.querySelector("#product").innerHTML = navigator.product;
+document.querySelector("#cookieenabled").innerHTML = navigator.cookieEnabled;
+document.querySelector("#appversion").innerHTML = navigator.appVersion;
+document.querySelector("#useragent").innerHTML = navigator.userAgent;
+document.querySelector("#language").innerHTML = navigator.language;
+document.querySelector("#online").innerHTML = navigator.onLine;
+document.querySelector("#javaenabled").innerHTML = navigator.javaEnabled();
+
