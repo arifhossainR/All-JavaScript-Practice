5cca083d117a2ae193b0bfa8d0a85ca2d7b75cdb
ArifHossainR <arifcse012@gmail.com>
2024-03-30 18:04:28 +0600

    getCurrentPosition and watchPosition

---
+++ webAPI/geolocation/getCurrentPosition.js
@@ -0,0 +1,30 @@
+const display = document.getElementById("display");
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    }else{
+        display.innerHTML = "This browser is not supported for geolocation !!!";
+    }
+}
+
+function showPosition(instantPosition){
+    display.innerHTML = "অক্ষ্যাংশ হলোঃ- " + instantPosition.coords.latitude + "<br>" + "দ্রাঘিমাংশ হলোঃ- " + instantPosition.coords.longitude;
+}
+
+function showError(Error){
+    switch(Error.code)
+    {
+        case Error.PERMISSION_DENIED:
+            display.innerHTML = "User denied the location permission.";
+            break;
+        case Error.POSITION_UNAVAILABLE:
+            display.innerHTML = "location information is unavailable.";
+            break;
+        case Error.TIMEOUT:
+            display.innerHTML = "The request to get user location timed out.";
+            break;
+        case Error.UNKNOWN_ERROR:
+            display.innerHTML = "An unknown error occurred";
+            break;
+    }
+}
